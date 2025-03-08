a683362e6844980c08bb76c3b82c2f0989d757fd
Henry Tsai <henry88002605@gmail.com>
2025-03-08 12:56:54 +1000

    split the blocks & test it

--- src/test_markdown_to_blocks.py
+++ src/test_markdown_to_blocks.py
@@ -1,23 +1,24 @@
 import unittest
-from markdown_to_blockes import markdown_to_blocks
+from markdown_to_blocks import markdown_to_blocks
 
 class TestMarkdownToHTML(unittest.TestCase):
     def test_markdown_to_blocks(self):
         markdown= """
 This is **bolded** paragraph
 
-This is another paragraph with *italic* text and `code` here
+This is another paragraph with _italic_ text and `code` here
 This is the same paragraph on a new line
 
-* This is a list
-* with items
+- This is a list
+- with items
 """
+        blocks = markdown_to_blocks(markdown)
         self.assertEqual(
-            markdown_to_blocks(markdown),
+            blocks,
             [
                 "This is **bolded** paragraph",
-                "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
-                "* This is a list\n* with items",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
             ],
         )
 
@@ -28,18 +29,19 @@ This is **bolded** paragraph
 
 
 
-This is another paragraph with *italic* text and `code` here
+This is another paragraph with _italic_ text and `code` here
 This is the same paragraph on a new line
 
-* This is a list
-* with items
+- This is a list
+- with items
 """
+        blocks = markdown_to_blocks(markdown)
         self.assertEqual(
-            markdown_to_blocks(markdown),
+            blocks,
             [
                 "This is **bolded** paragraph",
-                "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
-                "* This is a list\n* with items",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
             ],
         )
 
